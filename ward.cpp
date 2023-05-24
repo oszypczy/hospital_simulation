1a58414f1dc7588aa5f2d4e591c2621a3b7bcd84
Frytkk <andrzejtokajuk1@wp.pl>
2023-05-24 18:58:13 +0200

    Move object instead of creating copy

--- ward.cpp
+++ ward.cpp
@@ -1,7 +1,7 @@
 #include "ward.h"
 
-Ward::Ward(std::string id, std::string name, std::list<std::unique_ptr<Room>> roomList): 
-            Place(id), name(name), roomList(roomList){}
+Ward::Ward(std::string id, std::string name, std::list<std::unique_ptr<Room>> roomList):
+            Place(id), name(name), roomList(std::move(roomList)){}
 
 std::string Ward::getName() const{
     return name;
