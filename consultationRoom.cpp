f222e7397892b3bc9379987cc866ae0945b920e3
Frytkk <andrzejtokajuk1@wp.pl>
2023-05-25 19:05:14 +0200

    Class like treatmentRoom, but is with consultations

---
+++ consultationRoom.cpp
@@ -0,0 +1,121 @@
+#include "consultationRoom.h"
+#include "invalidHumanPointer.h"
+
+#include <algorithm>
+
+ConsultationRoom::ConsultationRoom(std::string id): Room(id){}
+
+
+void ConsultationRoom::addPatientToQueue(std::unique_ptr<Patient> patient){
+    patientsQueue.push_back(std::move(patient));
+}
+
+
+void ConsultationRoom::removePatient(std::unique_ptr<Patient> patient){
+    auto it = std::find_if(patientsQueue.begin(), patientsQueue.end(), [&](const std::unique_ptr<Patient>& p) {
+        return *p == *patient;
+    });
+
+    if (it != patientsQueue.end()) {
+        patientsQueue.erase(it);
+    }
+    else throw InvalidHumanPointer("Patient");
+}
+
+
+void ConsultationRoom::addDoctor(std::unique_ptr<Doctor> doctor){
+    doctors.push_back(std::move(doctor));
+}
+
+
+void ConsultationRoom::removeDoctor(std::unique_ptr<Doctor> doctor){
+    auto it = std::find_if(doctors.begin(), doctors.end(), [&](const std::unique_ptr<Doctor>& d) {
+        return *d == *doctor;
+    });
+
+    if (it != doctors.end()) {
+        doctors.erase(it);
+    }
+    else throw InvalidHumanPointer("Doctor");
+}
+
+
+void ConsultationRoom::addNurse(std::unique_ptr<Nurse> nurse){
+    nurses.push_back(std::move(nurse));
+}
+
+
+void ConsultationRoom::removeNurse(std::unique_ptr<Nurse> nurse){
+    auto it = std::find_if(nurses.begin(), nurses.end(), [&](const std::unique_ptr<Nurse>& n) {
+        return *n == *nurse;
+    });
+
+    if (it != nurses.end()) {
+        nurses.erase(it);
+    }
+    else throw InvalidHumanPointer("Nurse");
+}
+
+
+std::unique_ptr<Patient> ConsultationRoom::movePatient(std::unique_ptr<Patient> patient){
+    auto it = std::find_if(patientsQueue.begin(), patientsQueue.end(), [&](const std::unique_ptr<Patient>& p) {
+        return *p == *patient;
+    });
+
+    if (it != patientsQueue.end()) {
+        patientsQueue.erase(it);
+    }
+    else throw InvalidHumanPointer("Patient");
+
+    return std::move(*it);
+}
+
+std::unique_ptr<Doctor> ConsultationRoom::moveDoctor(std::unique_ptr<Doctor> doctor){
+    auto it = std::find_if(doctors.begin(), doctors.end(), [&](const std::unique_ptr<Doctor>& d) {
+        return *d == *doctor;
+    });
+
+    if (it != doctors.end()) {
+        doctors.erase(it);
+    }
+    else throw InvalidHumanPointer("Doctor");
+
+    return std::move(*it);
+}
+
+std::unique_ptr<Nurse> ConsultationRoom::moveNurse(std::unique_ptr<Nurse> nurse){
+    auto it = std::find_if(nurses.begin(), nurses.end(), [&](const std::unique_ptr<Nurse>& n) {
+        return *n == *nurse;
+    });
+
+    if (it != nurses.end()) {
+        nurses.erase(it);
+    }
+    else throw InvalidHumanPointer("Nurse");
+
+    return std::move(*it);
+}
+
+void ConsultationRoom::setConsultedPatient(std::unique_ptr<Patient> patient){
+    this->consultedPatient = std::move(patient);
+}
+
+std::unique_ptr<Patient> ConsultationRoom::moveConsultedPatient(){
+    return std::move(consultedPatient);
+}
+
+std::list<std::unique_ptr<Doctor>>& ConsultationRoom::getDoctors(){
+    return doctors;
+}
+
+std::list<std::unique_ptr<Nurse>>& ConsultationRoom::getNurses(){
+    return nurses;
+}
+
+std::unique_ptr<Patient>& ConsultationRoom::getConsultedPatient(){
+    return consultedPatient;
+}
+
+std::unique_ptr<Consultation>& ConsultationRoom::getConsultation(){
+    return consultation;
+}
---
+++ consultationRoom.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "room.h"
+#include "doctor.h"
+#include "nurse.h"
+#include "patient.h"
+#include "consultation.h"
+
+
+#include <memory>
+#include <list>
+#include <deque>
+#include <utility>
+
+class ConsultationRoom : public Room{
+    private:
+        std::list<std::unique_ptr<Doctor>> doctors;
+        std::list<std::unique_ptr<Nurse>> nurses;
+        std::deque<std::unique_ptr<Patient>> patientsQueue;
+        std::unique_ptr<Patient> consultedPatient;
+        std::unique_ptr<Consultation> consultation;
+    public:
+        ConsultationRoom(std::string id);
+        void addPatientToQueue(std::unique_ptr<Patient> patient);
+        void removePatient(std::unique_ptr<Patient> patient);
+        void addDoctor(std::unique_ptr<Doctor> doctor);
+        void removeDoctor(std::unique_ptr<Doctor> doctor);
+        void addNurse(std::unique_ptr<Nurse> nurse);
+        void removeNurse(std::unique_ptr<Nurse> nurse);
+
+        std::unique_ptr<Patient> movePatient(std::unique_ptr<Patient> patient);
+        std::unique_ptr<Doctor> moveDoctor(std::unique_ptr<Doctor> doctor);
+        std::unique_ptr<Nurse> moveNurse(std::unique_ptr<Nurse> nurse);
+
+        void setConsultedPatient(std::unique_ptr<Patient> patient);
+        std::unique_ptr<Patient> moveConsultedPatient();
+
+        std::list<std::unique_ptr<Doctor>>& getDoctors();
+        std::list<std::unique_ptr<Nurse>>& getNurses();
+        std::unique_ptr<Patient>& getConsultedPatient();
+        std::unique_ptr<Consultation>& getConsultation();
+};
