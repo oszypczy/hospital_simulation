48e39643f673208e4f9ce28e33da6b111db4b84f
Frytkk <andrzejtokajuk1@wp.pl>
2023-05-25 18:49:16 +0200

    Added getters

--- hospital.cpp
+++ hospital.cpp
@@ -1,55 +1,122 @@
 #include "hospital.h"
+#include "ward.h"
 #include "invalidHumanPointer.h"
 #include "invalidHospitalName.h"
+#include "invalidPlaceIDs.h"
 
 #include <algorithm>
 
-Hospital::Hospital(std::string name, std::list<std::unique_ptr<Place>> placesList,
-                    std::list<std::unique_ptr<Human>> humanList, std::unique_ptr<Reception> reception,
+Hospital::Hospital(std::string name, std::list<std::unique_ptr<Ward>> wardsList,
+                    std::list<std::unique_ptr<Doctor>> doctorsList, std::list<std::unique_ptr<Nurse>> nursesList,
+                    std::unique_ptr<Reception> reception,
                     std::unique_ptr<AmbulanceDispatch> ambulanceDispatch):
-                    name(name), placesList(std::move(placesList)), humansList(std::move(humanList)),
-                    reception(std::move(reception)), ambulanceDispatch(std::move(ambulanceDispatch))
+                    name(name), wardsList(std::move(wardsList)), doctorsList(std::move(doctorsList)),
+                    nursesList(std::move(nursesList)), reception(std::move(reception)),
+                    ambulanceDispatch(std::move(ambulanceDispatch))
                     {
                         if(name.empty()){
                             throw InvalidHospitalName();
+                        }
+                        if (!checkIfPlacesIDUnique()){
+                            throw InvalidPlaceIDs();
                         }
                     }
 
 bool Hospital::checkIfPlacesIDUnique()const{
     std::list<std::string> duplicatePlacesList;
 
-    for(const auto& place : placesList){
-        duplicatePlacesList.push_back(place->getID());
-    }
+    for(const auto& ward : wardsList){
+        duplicatePlacesList.push_back(ward->getID());
 
+        for(const auto& room : ward->getGeneralRoomList()){
+            duplicatePlacesList.push_back(room->getID());
+            }
+        duplicatePlacesList.push_back(ward->getTreatmentRoom()->getID());
+        duplicatePlacesList.push_back(ward->getConsultationRoom()->getID());
+        }
+
+    std::list<std::string> placesList = duplicatePlacesList;
     duplicatePlacesList.unique();
     return (duplicatePlacesList.size() == placesList.size());
 }
 
-void Hospital::addHuman(std::unique_ptr<Human> human){
-    humansList.push_back(std::move(human));
+
+void Hospital::addDoctor(std::unique_ptr<Doctor> doctor){
+    doctorsList.push_back(std::move(doctor));
 }
 
-void Hospital::removeHuman(std::unique_ptr<Human> human) {
-    auto it = std::find_if(humansList.begin(), humansList.end(), [&](const std::unique_ptr<Human>& h) {
-        return *h == *human;
-    });
-
-    if (it != humansList.end()) {
-        humansList.erase(it);
-    }
-    else throw InvalidHumanPointer("Human");
+void Hospital::addNurse(std::unique_ptr<Nurse> nurse){
+    nursesList.push_back(std::move(nurse));
 }
 
-std::unique_ptr<Human> Hospital::moveHuman(std::unique_ptr<Human> human){
-    auto it = std::find_if(humansList.begin(), humansList.end(), [&](const std::unique_ptr<Human>& h) {
-        return *h == *human;
+void Hospital::removeDoctor(std::unique_ptr<Doctor> doctor){
+    auto it = std::find_if(doctorsList.begin(), doctorsList.end(), [&](const std::unique_ptr<Doctor>& d) {
+        return *d == *doctor;
     });
 
-    if (it != humansList.end()) {
-        humansList.erase(it);
+    if (it != doctorsList.end()) {
+        doctorsList.erase(it);
     }
-    else throw InvalidHumanPointer("Patient");
+    else throw InvalidHumanPointer("Doctor");
+}
 
-    return std::move(*it);
+void Hospital::removeNurse(std::unique_ptr<Nurse> nurse){
+    auto it = std::find_if(nursesList.begin(), nursesList.end(), [&](const std::unique_ptr<Nurse>& n) {
+        return *n == *nurse;
+    });
+
+    if (it != nursesList.end()) {
+        nursesList.erase(it);
+    }
+    else throw InvalidHumanPointer("Nurse");
+}
+
+std::unique_ptr<Doctor> Hospital::moveDoctor(std::unique_ptr<Doctor> doctor){
+    auto it = std::find_if(doctorsList.begin(), doctorsList.end(), [&](const std::unique_ptr<Doctor>& d) {
+        return *d == *doctor;
+    });
+
+    if (it != doctorsList.end()) {
+        std::unique_ptr<Doctor> temp = std::move(*it);
+        doctorsList.erase(it);
+        return temp;
+    }
+    else throw InvalidHumanPointer("Doctor");
+}
+
+std::unique_ptr<Nurse> Hospital::moveNurse(std::unique_ptr<Nurse> nurse){
+    auto it = std::find_if(nursesList.begin(), nursesList.end(), [&](const std::unique_ptr<Nurse>& n) {
+        return *n == *nurse;
+    });
+
+    if (it != nursesList.end()) {
+        std::unique_ptr<Nurse> temp = std::move(*it);
+        nursesList.erase(it);
+        return temp;
+    }
+    else throw InvalidHumanPointer("Nurse");
+}
+
+std::list<std::unique_ptr<Ward>>& Hospital::getWardsList(){
+    return wardsList;
+}
+
+std::list<std::unique_ptr<Doctor>>& Hospital::getDoctorsList(){
+    return doctorsList;
+}
+
+std::list<std::unique_ptr<Nurse>>& Hospital::getNursesList(){
+    return nursesList;
+}
+
+std::unique_ptr<Reception>& Hospital::getReception(){
+    return reception;
+}
+
+std::unique_ptr<AmbulanceDispatch>& Hospital::getAmbulanceDispatch(){
+    return ambulanceDispatch;
+}
+
+std::string Hospital::getName()const{
+    return name;
 }
--- hospital.h
+++ hospital.h
@@ -1,9 +1,8 @@
 #pragma once
-#include "place.h"
-#include "human.h"
 #include "serviceDataBase.h"
 #include "reception.h"
 #include "ambulanceDispatch.h"
+#include "ward.h"
 
 #include <string>
 #include <list>
@@ -13,19 +12,32 @@
 class Hospital{
     private:
         std::string name;
-        std::list<std::unique_ptr<Place>> placesList;
-        std::list<std::unique_ptr<Human>> humansList;
+        std::list<std::unique_ptr<Ward>> wardsList;
+        std::list<std::unique_ptr<Doctor>> doctorsList;
+        std::list<std::unique_ptr<Nurse>> nursesList;
         std::unique_ptr<ServiceDataBase> serviceDataBase;
         std::unique_ptr<Reception> reception;
         std::unique_ptr<AmbulanceDispatch> ambulanceDispatch;
     public:
-        Hospital(std::string name, std::list<std::unique_ptr<Place>> placesList,
-                std::list<std::unique_ptr<Human>> humansList,
+        Hospital(std::string name, std::list<std::unique_ptr<Ward>> wardsList,
+                std::list<std::unique_ptr<Doctor>> doctorsList,
+                std::list<std::unique_ptr<Nurse>> nursesList,
                 std::unique_ptr<Reception> reception,
                 std::unique_ptr<AmbulanceDispatch> ambulanceDispatch);
         bool checkIfPlacesIDUnique() const;
-        void addHuman(std::unique_ptr<Human> human);
-        void removeHuman(std::unique_ptr<Human> human);
+        void addDoctor(std::unique_ptr<Doctor> doctor);
+        void addNurse(std::unique_ptr<Nurse> nurse);
+        void removeDoctor(std::unique_ptr<Doctor> doctor);
+        void removeNurse(std::unique_ptr<Nurse> nurse);
 
-        std::unique_ptr<Human> moveHuman(std::unique_ptr<Human> human);
+        std::unique_ptr<Doctor> moveDoctor(std::unique_ptr<Doctor> doctor);
+        std::unique_ptr<Nurse> moveNurse(std::unique_ptr<Nurse> nurse);
+
+        std::list<std::unique_ptr<Ward>>& getWardsList();
+        std::list<std::unique_ptr<Doctor>>& getDoctorsList();
+        std::list<std::unique_ptr<Nurse>>& getNursesList();
+        std::unique_ptr<Reception>& getReception();
+        std::unique_ptr<AmbulanceDispatch>& getAmbulanceDispatch();
+        std::unique_ptr<ServiceDataBase>& getServiceDataBase();
+        std::string getName() const;
 };
